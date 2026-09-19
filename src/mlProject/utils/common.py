54d8754308668ee2b7f74bf19ab6299ba48d7998
Zonaira-Munir <zonairamunir@outlook.com>
2026-09-20 00:07:50 +0500

    data ingestion added

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -1,11 +1,11 @@
 import os
+from box import ConfigBox
 from box.exceptions import BoxValueError
 import yaml
 from src.mlProject import logger
 import json
-import joblib
+import joblib 
 from ensure import ensure_annotations
-from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
